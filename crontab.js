478d3790803d0a6d93b618284ba6184a947b7290
Masunaga Ray <ray@msng.info>
2013-12-19 08:30:54 +0900

    reformat

--- crontab.js
+++ crontab.js
@@ -4,11 +4,11 @@ $(function () {
     var submitButtonId = 'check';
     var daysOfWeek = '日月火水木金土日';
     var rangeOfUnits = {
-        '分':[0,59],
-        '時':[0,23],
-        '日':[1,31],
-        '月':[1,12],
-        '曜日':[0,6]
+        '分': [0, 59],
+        '時': [0, 23],
+        '日': [1, 31],
+        '月': [1, 12],
+        '曜日': [0, 6]
     };
 
     $('#' + submitButtonId).on('click', function () {
@@ -71,7 +71,7 @@ $(function () {
                     return '<span class="error">エラー: "' + splitElements[i] + '"</span>';
                 }
                 for (var j = 0; j < subElements.length; j++) {
-                    if (subElements[j]<rangeOfUnit[0]||subElements[j]>rangeOfUnit[1]){
+                    if (subElements[j] < rangeOfUnit[0] || subElements[j] > rangeOfUnit[1]) {
                         return '<span class="error">out of range: "' + splitElements[i] + '"</span>';
                     }
                 }
