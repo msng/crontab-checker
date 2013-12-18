385f730aa9e07e71a1cd6a50f654c48f534bc6e1
mogya <mogya@mogya.com>
2013-12-18 21:14:25 +0900

    数値の範囲チェック機能を追加

--- crontab.js
+++ crontab.js
@@ -3,6 +3,13 @@ $(function () {
     var resultId = 'result';
     var submitButtonId = 'check';
     var daysOfWeek = '日月火水木金土日';
+    var rangeOfUnits = {
+        '分':[0,59],
+        '時':[0,23],
+        '日':[1,31],
+        '月':[1,12],
+        '曜日':[0,6]
+    };
 
     $('#' + submitButtonId).on('click', function () {
         var crontab = $('#' + crontabInputId).val();
@@ -50,6 +57,7 @@ $(function () {
     function describeElement(elements, unit, convert) {
         var splitElements = elements.split(',');
         var result = '';
+        var rangeOfUnit = rangeOfUnits[unit];
         for (var i = 0; i < splitElements.length; i++) {
             if (splitElements[i] == '*') {
                 result += '毎' + unit;
@@ -61,6 +69,11 @@ $(function () {
                 var subElements = splitElements[i].split('-');
                 if (subElements.length > 2) {
                     return '<span class="error">エラー: "' + splitElements[i] + '"</span>';
+                }
+                for (var j = 0; j < subElements.length; j++) {
+                    if (subElements[j]<rangeOfUnit[0]||subElements[j]>rangeOfUnit[1]){
+                        return '<span class="error">out of range: "' + splitElements[i] + '"</span>';
+                    }
                 }
                 if (convert) {
                     subElements[0] = convert.charAt(subElements[0]);
